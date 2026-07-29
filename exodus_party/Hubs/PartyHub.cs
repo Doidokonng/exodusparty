7a3a95921c42b9216280de8004490335e8d9f6a2
Deivison Santos <deivisonsantos220@gmail.com>
2026-07-29 20:15:09 -0300

    YouTube player (iframe) update

--- exodus_party/Hubs/PartyHub.cs
+++ exodus_party/Hubs/PartyHub.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace exodus_party.Hubs
 {
     public class PartyHub : Hub
     {
+        public async Task PausarVideo(int partyId)
+        {
+            await Clients.All.SendAsync("ReceberComandoPause", partyId);
+        }
+
+        public async Task DarPlayVideo(int partyId)
+        {
+            await Clients.All.SendAsync("ReceberComandoPlay", partyId);
+        }
     }
 }
